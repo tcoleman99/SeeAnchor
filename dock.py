5bf0ff4a10e9377504cc7c0ea5beccd8b005d6db
Tanner Coleman <tcoleman@jackhosp.org>
2022-06-13 14:46:15 -0500

    Note buttons can now be rearranged

--- dock.py
+++ dock.py
@@ -1,11 +1,13 @@
 from asyncio.windows_events import NULL
-from cgi import test
+from http.client import NOT_ACCEPTABLE
+from mimetypes import common_types
+from time import sleep
 import tkinter as tk
 import tkinter.ttk as ttk
-from tkinter import E, N, NE, NW, TOP, W, Toplevel, messagebox
+from tkinter import E, N, NE, NW, TOP, W, Toplevel
 from tkinter.scrolledtext import ScrolledText
 import random
-from turtle import bgcolor
+from functools import partial
 
 class Dock(object):
 
@@ -53,24 +55,19 @@ class Dock(object):
         self.buttonFrame.columnconfigure(1, weight=2)
         self.buttonFrame.columnconfigure(2, weight=2)
 
-        self.folderFrame = tk.Frame(self.root, width=280, height=400) # w=280, h=400
-        self.folderFrame.grid_propagate(False)
-        self.folderFrame.pack()
-        self.folderFrame.columnconfigure(0, weight=1)
-        self.folderFrame.columnconfigure(1, weight=2)
-        self.folderFrame.columnconfigure(2, weight=1)
-
         # Creating scrollable canvas for note buttons to reside on
-        self.canvas = tk.Canvas(self.folderFrame, width=265, height=365, bg="turquoise1")
+        self.folderFrame = tk.Frame(self.root, width=280, height=400, bg="blue") # w=280, h=400
+        self.canvas = tk.Canvas(self.folderFrame, width=265, height=365, bg="blue")
         self.canvas.bind_all("<MouseWheel>", self._on_mousewheel)
+
         scrollbar = ttk.Scrollbar(self.folderFrame, orient="vertical", command=self.canvas.yview)
-        scrollable_frame = tk.Frame(self.canvas, width=265, height=365, bg="lime green")
+        scrollable_frame = tk.Frame(self.canvas, width=265, height=365, bg="grey69")
+        scrollable_frame.columnconfigure(0, weight=1)
 
         scrollable_frame.bind("<Configure>", lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
         self.canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
         self.canvas.configure(yscrollcommand=scrollbar.set)
-
-        self.folderFrame.pack(padx=(20, 20))
+        self.folderFrame.pack() # padx=(20, 20)
         self.canvas.pack(side="left", fill="both", expand=True)
         scrollbar.pack(side="right", fill="y")
 
@@ -92,13 +89,13 @@ class Dock(object):
         delNote = tk.Button(self.buttonFrame, text="Edit", width=5, command=lambda: self.set_editable(), relief="flat")
         delNote.grid(column=1, row=0, sticky=E, pady=(15,0))
 
-        # Sets the app to always be in foreground
+        # Sets the app to always be in foreground and runs
         self.root.attributes("-topmost", True)
         self.root.update()
         self.root.mainloop()
     
-    # ----------------------------------FUNCTIONS--------------------------------------
-
+    # ============================FUNCTIONS=============================
+    # ----------Title Frame-----------
     # Minimize button
     def min_but(self, scw, sch, maxb, tf, bf, ff):
         self.root.geometry('%dx%d+%d+%d' % (25, 25, (scw - 50), (sch - 65)))
@@ -122,17 +119,6 @@ class Dock(object):
         ff.pack(padx=(22.5, 22.5))
         self.root.update_idletasks()
 
-    # Create a new note, makes new entries in lists, makes button draggable if edit is enabled
-    def new_note(self, frame, index):   # Have to pass self.count as index or else self.pads[self.count] does not accept input
-        self.pads.append(Notepad(self, self.count))
-        self.label = tk.Button(frame, text="New label #%d" % (index), width=36, command=lambda: [self.pads[index].openOverride()], relief="flat")
-        self.label.grid(column=0, row=self.count, sticky=tk.E, pady=(0, 10))
-        if(self.editable == True):
-            self.label["state"] = "disabled"
-            Dock.make_draggable(self.label)
-        self.notepad_list.append(self.label)
-        self.count += 1
-
     # Saves and destroys all notes when closing dock
     def close_help(self):
         for x in self.pads:
@@ -141,47 +127,122 @@ class Dock(object):
         self.notepad_list.clear()
         self.root.destroy()
 
+    # ----------Button Frame----------
+    # Create a new note, makes new entries in lists, makes button draggable if edit is enabled
+    def new_note(self, frame, index):   # Have to pass self.count as index or else self.pads[self.count] does not accept input
+        self.pads.append(Notepad(self, self.count))
+        # testframe = tk.Frame(frame, width=265, height=25, bg="red")
+        # testframe.pack_propagate(False) # Pack_propagate works but grid_propagate doesn't when using grid to display frame??
+        # testframe.grid(column=0, row=self.count, sticky=tk.E, pady=(0, 10))
+        self.label = tk.Button(frame, text="New label #%d" % (index), width=36, command=lambda: [self.pads[index].openOverride(), ], relief="flat")
+        self.label.grid(column=0, columnspan=2, row=self.count, sticky=tk.N+tk.S+tk.E+tk.W, pady=(0, 10))
+        if(self.editable == True):
+            self.label["state"] = "disabled"
+            #Dock.make_draggable(self.label)
+        self.notepad_list.append(self.label)
+        self.count += 1
+
+    # ----------Drag & Drop-----------
     # For drag and drop of labels in dock
+
     def set_editable(self):
         if(len(self.notepad_list) > 0 and self.editable == False):
-            for x in self.notepad_list:
-                x["state"] = "disabled"
-                Dock.make_draggable(x)
+            for i in range(len(self.notepad_list)):
+                self.notepad_list[i].configure(command=lambda idx=i: self.set_select(self.notepad_list[idx]))
+                # if(i==0):
+                #     self.notepad_list[0].configure(command=lambda: Dock._selected(self.notepad_list[0]))
+                #self.notepad_list[i].configure(command=lambda: Dock._selected(self.notepad_list[i]))
+                #print(self.notepad_list[i].cget("text"))
+                #Dock._selected(self.notepad_list[i])
+                # self.label.configure(command=lambda: Dock._selected(self.label))
+            # for x in self.notepad_list:
+            #     x["state"] = "disabled"
+            #     Dock.make_draggable(x)
             self.editable = True
-            print(str(self.editable) + " if")
         elif(len(self.notepad_list) > 0 and self.editable == True):
-            for x in self.notepad_list:
-                Dock.make_undraggable(x)
-                x["state"] = "normal"
-            print(str(self.editable) + " elif")
+            # for x in self.notepad_list:
+            #     Dock.make_undraggable(x)
+            #     x["state"] = "normal"
+            for i in range(len(self.notepad_list)):
+                self.notepad_list[i].configure(command=lambda idx=i: self.pads[idx].openOverride())
             self.editable = False
         else:
             self.editable = False
-            print(str(self.editable) + " else")
-
-    def make_draggable(widget):
-        widget.bind("<Button-1>", Dock.on_drag_start)
-        widget.bind("<B1-Motion>", Dock.on_drag_motion)
-        widget.configure(width=40) # 273
-
-    def make_undraggable(widget):
-        widget.unbind("<Button-1>")
-        widget.unbind("<B1-Motion>")
-
-    def on_drag_start(event):
-        widget = event.widget
-        widget._drag_start_y = event.y
-
-    def on_drag_motion(event):
-        widget = event.widget
-        y = widget.winfo_y() - widget._drag_start_y + event.y
-        if(y < 0):
-            widget.place(y=0)
-        else:
-            widget.place(y=y)
-
+    # Sets all other button     
     def _on_mousewheel(self, event):
         self.canvas.yview_scroll(int(-1*(event.delta/120)), "units")
+    
+    def set_select(self, target):
+        target.configure(bg="Yellow")
+        for i in range(len(self.notepad_list)):
+            self.notepad_list[i].configure(command=lambda idx=i: self.switch(self.notepad_list[idx]))
+
+    def switch(self, target):
+        target.configure(bg="Green")
+        for i in self.notepad_list:
+            if(i.cget("bg") == "Yellow"):
+                first_target = i
+            if(i.cget("bg") == "Green"):
+                second_target = i
+            else:
+                print("*")
+        row1 = first_target.grid_info()["row"]
+        row2 = second_target.grid_info()["row"]
+        first_target.grid(row=row2)
+        second_target.grid(row=row1)
+        for i in range(len(self.notepad_list)):
+            self.notepad_list[i].configure(bg="white")
+            self.notepad_list[i].configure(command=lambda idx=i: self.set_select(self.notepad_list[idx]))
+        
+        
+    # def make_draggable(widget):
+    #     widget.bind("<Button-1>", Dock.on_drag_start)
+    #     widget.bind("<B1-Motion>", Dock.on_drag_motion)
+    #     widget.bind("<ButtonRelease-1>", Dock.snap)
+    #     widget.configure(width=30) # 273
+    #     print(widget.grid_info())
+
+    # def make_undraggable(widget):
+    #     widget.unbind("<Button-1>")
+    #     widget.unbind("<B1-Motion>")
+
+    # def on_drag_start(event):
+    #     widget = event.widget
+    #     widget.grid
+    #     widget.lift()
+    #     widget._drag_start_y = event.y
+
+    # def on_drag_motion(event):
+    #     widget = event.widget
+    #     y = widget.winfo_y() - widget._drag_start_y + event.y
+    #     if(y < 0):
+    #         widget.place(y=0)
+    #     else:
+    #         widget.place(y=y)
+    #     print(y)
+
+    # def snap(event):
+    #     widget = event.widget
+    #     boundary_one = (widget.winfo_y() // 35) * 35
+    #     print("B1: " + str(boundary_one))
+    #     boundary_two = boundary_one + 35
+    #     print("B2: " + str(boundary_two))
+    #     if((boundary_one <= widget.winfo_y() <= boundary_two)):
+    #         #widget.place(y=boundary_one)
+    #         widget.grid(column=0, columnspan=2, row=(widget.winfo_y() // 35), sticky=tk.N+tk.S+tk.E+tk.W, pady=(0, 10))
+    #     Dock.func(widget)
+
+    # def rearrange(self):
+    #     for i in range(len(self.notepad_list)):
+    #         for x in range(len(self.notepad_list)):
+    #             if(i == x):
+    #                 pass
+    #             elif(self.notepad_list[i] == self.notepad_list[x]):
+    #                 self.notepad_list[x] = max(self.notepad_list) + 1
+    #             else:
+    #                 print("k")
+    #             self.notepad_list.sort()
+    #             print(self.notepad_list)
 
 class Notepad(Dock):
 
