5e979e74d372c8fb33002cf6c44f17a1acb4a135
Tanner Coleman <tcoleman@jackhosp.org>
2022-07-08 15:19:46 -0500

    Starting reminder thread implementation research

--- dock.py
+++ dock.py
@@ -5,6 +5,8 @@ from tkinter import CENTER, E, N, NE, NW, TOP, W, Menu, Toplevel
 from tkinter.scrolledtext import ScrolledText
 import random
 from functools import partial
+from plyer import notification
+from datetime import datetime
 
 class Dock(object):
 
@@ -18,23 +20,23 @@ class Dock(object):
         self.editable = False
 
         # Getting screen size
-        screen_width = self.root.winfo_screenwidth()
-        screen_height = self.root.winfo_screenheight()
+        self.screen_width = self.root.winfo_screenwidth()
+        self.screen_height = self.root.winfo_screenheight()
 
         # Original GUI size is 325x500
-        w = 325
-        h = 500
+        self.w = 325
+        self.h = 500
 
         # Setting position of GUI on start up (subtract 40 for the taskbar)
-        x = screen_width - w
-        y = screen_height - h - 40
+        self.x = self.screen_width - self.w
+        self.y = self.screen_height - self.h - 40
 
         # For getting size of current window?
         self.root.update_idletasks()
 
         # Setting up frames for root window, title, buttons, and notepad list
         self.root.overrideredirect(True)
-        self.root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+        self.root.geometry('%dx%d+%d+%d' % (self.w, self.h, self.x, self.y))
         self.root.configure(bg="gray36")
         self.root.resizable(width=False, height=False)
 
@@ -58,20 +60,20 @@ class Dock(object):
         # self.canvas.bind_all("<MouseWheel>", self._on_mousewheel)
 
         scrollbar = ttk.Scrollbar(self.folderFrame, orient="vertical", command=self.canvas.yview, )
-        scrollable_frame = tk.Frame(self.canvas, width=265, height=365, bg="grey69")
-        scrollable_frame.columnconfigure(0, weight=1)
+        self.scrollable_frame = tk.Frame(self.canvas, width=265, height=365, bg="grey69")
+        self.scrollable_frame.columnconfigure(0, weight=1)
 
-        scrollable_frame.bind("<Enter>", self.mousewheel_bound)
-        scrollable_frame.bind("<Leave>", self.mousewheel_unbound)
-        scrollable_frame.bind("<Configure>", lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
-        self.canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
+        self.scrollable_frame.bind("<Enter>", self.mousewheel_bound)
+        self.scrollable_frame.bind("<Leave>", self.mousewheel_unbound)
+        self.scrollable_frame.bind("<Configure>", lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
+        self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
         self.canvas.configure(yscrollcommand=scrollbar.set)
         self.folderFrame.pack() # padx=(20, 20)
         self.canvas.pack(side="left", fill="both", expand=True)
         scrollbar.pack(side="right", fill="y")
 
         # Button creation
-        minimize = tk.Button(self.titleFrame, text="\/", width=31, command=lambda: self.min_but(screen_width, screen_height, maximize, self.titleFrame, self.buttonFrame, self.folderFrame), bg="medium spring green", relief="flat")
+        minimize = tk.Button(self.titleFrame, text="\/", width=31, command=lambda: self.min_but(), bg="medium spring green", relief="flat")
         minimize.pack(side=tk.TOP)
 
         close = tk.Button(self.titleFrame, text="X", width=6, bg="firebrick1", command=lambda: self.close_help(), relief="flat")
@@ -80,17 +82,29 @@ class Dock(object):
         settingsBut = tk.Button(self.titleFrame, text="O", width=6, bg="turquoise1", command=lambda: self.testfun(), relief="flat")
         settingsBut.place(x=0)
 
-        maximize = custom_button(self.root, text="/\\",command=lambda: self.max_but(w, h, x, y, maximize, self.titleFrame, self.buttonFrame, self.folderFrame), bg="medium spring green", relief="flat", font=("BOLD, 12"))
-        maximize.menu.add_command(label="Color", command=partial(maximize.change_color, maximize))
-        maximize.menu.add_command(label="Font Color", command=partial(maximize.font_color, maximize))
-        maximize.menu.add_command(label="Unlock", command=partial(maximize.move_x_axis, maximize))
-        maximize.bind("<Button-3>", maximize.color_popup)
+        self.maximize = custom_button(self.root, text="/\\",command=lambda: self.max_but(), bg="medium spring green", relief="flat")
+        self.maximize.menu.add_command(label="Color", command=partial(self.maximize.change_color, self.maximize))
+        self.maximize.menu.add_command(label="Font Color", command=partial(self.maximize.font_color, self.maximize))
+        self.maximize.menu.add_separator()
+        self.maximize.menu.add_command(label="Unlock", command=partial(self.maximize.anchor_unlock, self.maximize, self.root, self.maximize.x_location))
+        self.maximize.bind("<Button-3>", self.maximize.color_popup)
 
-        newNote = tk.Button(self.buttonFrame, text="+", width=5, command=lambda: self.new_note(scrollable_frame, self.count), relief="flat")
+        newNote = tk.Button(self.buttonFrame, text="+", width=5, command=lambda: self.new_note(self.count), relief="flat")
         newNote.grid(column=1, row=0, sticky=W, pady=(15,0))
 
-        delNote = tk.Button(self.buttonFrame, text="Edit", width=5, command=lambda: self.set_editable(delNote), relief="flat")
-        delNote.grid(column=1, row=0, sticky=E, pady=(15,0))
+        delNote = tk.Button(self.buttonFrame, text="Trash", width=5, command=lambda: self.delete_note(), relief="flat")
+        delNote.grid(column=1, row=0, sticky=N, pady=(15, 0))
+
+        edit = tk.Button(self.buttonFrame, text="Edit", width=5, command=lambda: self.set_editable(edit), relief="flat")
+        edit.grid(column=1, row=0, sticky=E, pady=(15,0))
+
+        # self.reminder()
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        print(current_time)
+        if(current_time == "15:10:00"):
+            self.reminder()
+
 
         # Sets the app to always be in foreground and runs
         self.root.attributes("-topmost", True)
@@ -100,27 +114,30 @@ class Dock(object):
     # ============================FUNCTIONS=============================
     # ----------Title Frame-----------
     # Minimize button
-    def min_but(self, scw, sch, maxb, tf, bf, ff):
-        # self.root.geometry('%dx%d+%d+%d' % (25, 25, (scw - 50), (sch - 65)))
-        self.root.geometry('%dx%d+%d+%d' % (1920, 25, 0, (sch - 65)))
+    def min_but(self):
+        self.root.geometry('%dx%d+%d+%d' % (self.screen_width - self.maximize.winfo_x(), 25, self.maximize.winfo_x(), (self.screen_height - 65)))
+        self.root.configure(bg="#add123")
+        self.maximize.place(x=self.maximize.x_location, y=0, width=27)
+        self.root.geometry('%dx%d+%d+%d' % (self.screen_width - self.maximize.x_location, 25, self.maximize.x_location, (self.screen_height - 65)))
+        self.maximize.place(x=0)
+        self.root.wm_attributes("-transparentcolor", "#add123")
         self.root.overrideredirect(True)
-        tf.pack_forget()
-        bf.pack_forget()
-        ff.pack_forget()
+        self.titleFrame.pack_forget()
+        self.buttonFrame.pack_forget()
+        self.folderFrame.pack_forget()
         for x in self.notepads:
-            x.closeOverride()
-        
-        maxb.pack(side="top")
+            x.closeOverride()  
         self.root.update_idletasks()
 
     # Maximize button
-    def max_but(self, wi, hi, xi, yi, maxb, tf, bf, ff):
+    def max_but(self):
         self.root.overrideredirect(True)
-        self.root.geometry('%dx%d+%d+%d' % (wi, hi, xi, yi))
-        maxb.pack_forget()
-        tf.pack(side="top")
-        bf.pack()
-        ff.pack()
+        self.root.geometry('%dx%d+%d+%d' % (self.w, self.h, self.x, self.y))
+        self.root.configure(bg="gray36")
+        self.maximize.place_forget()
+        self.titleFrame.pack(side="top")
+        self.buttonFrame.pack()
+        self.folderFrame.pack()
         self.root.update_idletasks()
 
     # Saves and destroys all notes when closing dock
@@ -133,13 +150,15 @@ class Dock(object):
 
     # ----------Button Frame----------
     # Create a new note, makes new entries in lists, makes button draggable if edit is enabled
-    def new_note(self, frame, index):   # Have to pass self.count as index or else self.notepads[self.count] does not accept input
+    def new_note(self, index):   # Have to pass self.count as index or else self.notepads[self.count] does not accept input
         self.notepads.append(Notepad(self, self.count))
         self.notepads[index].closeOverride()
-        self.notepadButton = custom_button(frame, text="New label #%d" % (index), width=37, command=lambda: [self.notepads[index].openOverride()], relief="flat")
+        self.notepadButton = custom_button(self.scrollable_frame, text="New label #%d" % (index), width=37, command=lambda: [self.notepads[index].openOverride()], relief="flat")
         self.notepadButton.menu.add_command(label="Background Color", command=partial(self.notepadButton.change_color, self.notepadButton))
         self.notepadButton.menu.add_command(label="Font Color", command=partial(self.notepadButton.font_color, self.notepadButton))
-        self.notepadButton.grid(column=0, columnspan=2, row=self.count, sticky=tk.N+tk.S+tk.E+tk.W, pady=(0, 10))
+        self.notepadButton.menu.add_separator()
+        self.notepadButton.menu.add_command(label="Delete", command=partial(self.delete_note, self.notepadButton))
+        self.notepadButton.grid(column=0, columnspan=2, row=self.count+1, sticky=tk.N+tk.S+tk.E+tk.W, pady=(0, 10))
         self.notepadButton.bind("<Button-3>", self.notepadButton.color_popup)
         self.button_list.append(self.notepadButton)
         self.count += 1
@@ -148,11 +167,31 @@ class Dock(object):
                 if(note.selected == True):
                     selectedBut = note  # Using note seems to only use the last index in list instead of note with selected == True
                     self.notepadButton.configure(command=lambda: self.swap(selectedBut, self.notepadButton))
-                    print(note.cget("text"))
-                    print(self.notepadButton.cget("text"))
-                    print("2")
-                else:
-                    print("pass")
+
+    def delete_note(self, button):
+        # win = tk.Toplevel()
+        # win.wm_title("Delete Note")
+        # warning = tk.Label(win, text="Are you sure you want to delete this note? You will not be able to retrieve this note.", fg="Red")
+        # warning.pack(side="top")
+        # yesButton = tk.Button(win, text="Yes")
+        # yesButton.pack(side="left")
+        # noButton = tk.Button(win, text="No")
+        # noButton.pack(side="right")
+        self.count -= 1
+        self.toDelete = None
+        button.grid_forget()
+        for l in range(len(self.button_list)):
+            if(self.button_list[l] == button):
+                self.toDelete = l
+        del self.button_list[self.toDelete]
+        del self.notepads[self.toDelete]
+        for x in self.notepads[self.toDelete:]:
+            x.titleLabel.row -= 1
+        for i in self.button_list[self.toDelete:]:
+            i.grid(row=i.grid_info()["row"] - 1)
+            i.configure(command=lambda idx=self.toDelete: self.notepads[idx].openOverride())
+            self.toDelete += 1
+        self.root.update()
 
     # ----------Edit Note List Order-----------
     # For editing positions of notes in list on dock
@@ -176,15 +215,10 @@ class Dock(object):
     def first_selection(self, target1):
         target1.selected = True
         for i in range(len(self.button_list)):
-            print(i)
             self.button_list[i].configure(command=lambda idx=i: self.swap(target1, self.button_list[idx]))
         while(target1.selected == True):
             self.root.update()
             self.root.after(600, target1.configure(bg="royal blue"))
-            self.root.update()
-            self.root.after(600, target1.configure(bg="cyan2"))
-            self.root.update()
-            self.root.after(600, target1.configure(bg="SpringGreen1"))
             self.root.update()
             self.root.after(700, target1.configure(bg=target1.bgcolor))
             self.root.update()
@@ -221,29 +255,33 @@ class Dock(object):
             else:
                 print(i.cget("text") + " is NOT")
         print("---------")
+        for i in self.notepads:
+            print(i.titleLabel.cget("text"))
+
+    def reminder(self):
+        notification.notify(title="Break", message="take a break", timeout=1)
 
 class Notepad(Dock):
 
     def __init__(self, parent, position):
         # the root widget
         self.top = Toplevel()
-        self.starting_x = 1200
+        self.starting_x = 1260
         self.starting_y = 600
         self.top.geometry("325x325+%d+%d" % (self.starting_x, self.starting_y))
         self.top.title("Notepad")
         self.top.configure(bg="seashell2")
         self.top.resizable(width=True, height=True)
         self.top.protocol("WM_DELETE_WINDOW", self.closeOverride)
-        self.top.resizable(0,0)
-        self.position = position
+        # self.top.resizable(0,0)
         self.parent = parent
 
         self.filename = "file_" + str(random.randrange(10000, 50000))
 
         # Creating label
-        self.titleLabel = create_label(self.top, text="Title", bg="light blue", font=("Times 20 italic bold"))
+        self.titleLabel = create_label(self.top, text="Title %d" % (position), bg="light blue", font=("Times 20 italic bold"))
         self.titleLabel.newParent = parent    # To set the main application as the parent of create_label
-        self.titleLabel.row = self.position
+        self.titleLabel.row = position
         self.titleLabel.pack(side="top", fill="x")
 
         # Padding text within the main text box (left, top, right, bottom)
@@ -259,7 +297,7 @@ class Notepad(Dock):
         self.top.withdraw()
 
     def openOverride(self):
-        self.titleLabel.configure(bg=self.parent.button_list[self.position].cget("bg"))
+        self.titleLabel.configure(bg=self.parent.button_list[self.titleLabel.row].cget("bg"))
         self.saveFile()
         self.top.deiconify()
 
@@ -303,31 +341,51 @@ class custom_button(tk.Button):
         tk.Button.__init__(self, *args, **kwargs)
         self.menu = tk.Menu(self, tearoff=0)
 
+        self.x_location = 1750
         self.locked = True
         self.selected = False
         self.bgcolor = self.cget("bg")
         self.configure(bg=self.bgcolor)
 
 
-    def change_color(self, but):
+    def change_color(self, button):
         colors = askcolor(title="Background Color")
         self.bgcolor = colors[1]
-        but.configure(bg=self.bgcolor)
+        button.configure(bg=self.bgcolor)
 
-    def font_color(self, but):
+    def font_color(self, button):
         colors = askcolor(title="Font Color")
-        but.configure(fg=colors[1])
+        button.configure(fg=colors[1])
 
-    def move_x_axis(self, but):
-        but.locked = False
-        but["state"] = "disabled"
-        custom_button.make_draggable(but)
+    def anchor_unlock(self, button, window, location):
+        button.locked = False
+        window.geometry('%dx%d+%d+%d' % (window.winfo_screenwidth(), 25, 0, window.winfo_screenheight() - 65))
+        button.place(x=location)
+        button["state"] = "disabled"
+        button.menu.delete("Unlock")
+        button.menu.delete("Color")
+        button.menu.delete("Font Color")
+        button.menu.delete(0)   # Removes the separator line from the right click menu
+        button.menu.add_command(label="Lock", command=partial(button.anchor_lock, button, window))
+        custom_button.make_draggable(button)
+
+    def anchor_lock(self, button, window):
+        button.locked = True
+        button.x_location = button.winfo_x()
+        tmp_x = button.x_location
+        window.geometry("%dx%d+%d+%d" % (window.winfo_screenwidth() - int(button.x_location), 25, int(button.x_location), window.winfo_screenheight() - 65))
+        button.place(x=0)
+        button["state"] = "normal"
+        button.menu.delete("Lock")
+        button.menu.add_command(label="Color", command=partial(button.change_color, button))
+        button.menu.add_command(label="Font Color", command=partial(button.font_color, button))
+        button.menu.add_separator()
+        button.menu.add_command(label="Unlock", command=partial(button.anchor_unlock, button, window, tmp_x))
+        custom_button.make_undraggable(button)
 
     def make_draggable(widget):
         widget.bind("<Button-1>", custom_button.on_drag_start)
         widget.bind("<B1-Motion>", custom_button.on_drag_motion)
-        # widget.configure(width=30) # 273
-        #print(widget.grid_info())
 
     def make_undraggable(widget):
         widget.unbind("<Button-1>")
@@ -344,11 +402,10 @@ class custom_button(tk.Button):
         x = widget.winfo_x() - widget._drag_start_x + event.x
         if(x < 0):
             widget.place(x=0)
-        elif(x > 1897):
-            widget.place(x=1897)
+        elif(x > (event.widget.master.winfo_screenwidth() - 25)):
+            widget.place(x=(event.widget.master.winfo_screenwidth() - 25))
         else:
             widget.place(x=x)
-        print(x)
 
     def color_popup(self, event):
         try:
